b58a5a37fa5f1ac92a7bcbf01bc381be5b132b63
김형익 <kiomnd2@naver.com>
2024-05-10 14:36:28 +0900

    eureka client 세팅

--- hotel-internal-api/src/main/java/com/subprj/HotelApiApplication.java
+++ hotel-internal-api/src/main/java/com/subprj/HotelApiApplication.java
@@ -2,12 +2,14 @@ package com.subprj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.PropertySource;
 
 @PropertySource(ignoreResourceNotFound = true, encoding = "UTF-8", value = {
         "classpath:config/application-hotelapi.properties",
         "classpath:config/application-hotelapi-${spring.active.profiles}.properties"
 })
+@EnableDiscoveryClient
 @SpringBootApplication
 public class HotelApiApplication {
     public static void main(String[] args) {
