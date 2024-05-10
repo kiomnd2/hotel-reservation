04ae397b21f1d7aa8fa8c553ca0ceaad16f0e73e
김형익 <kiomnd2@naver.com>
2024-05-10 14:25:31 +0900

    eureka server 세팅

---
+++ eureka-server/src/main/java/com/subprj/eureka/EurekaApplication.java
@@ -0,0 +1,17 @@
+package com.subprj.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.PropertySource;
+
+@PropertySource(value = {
+        "classpath:config/application-eureka.properties"
+})
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class);
+    }
+}
